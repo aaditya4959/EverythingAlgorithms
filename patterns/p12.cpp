ef3a61571ac7cb3b2fe6957bf48f8a3ba327295f
Aaditya Bir Singh <aadi.bir.singh@gmail.com>
2026-05-28 23:05:53 +0530

    12 done

--- patterns/p12.cpp
+++ patterns/p12.cpp
@@ -1,1 +1,25 @@
-Haha
+#include <iostream>
+using namespace std;
+
+int main () {
+    int row;
+    cout<<"Enter the number of rows: ";
+    cin>>row;
+
+    int maxChar = 2*row;
+
+    for(int i = 1; i <=row ; i++){
+        int number = 1;
+        for(int j = 1; j<=i ; j++){
+            cout<<number++;
+        }
+        for(int j = i+1; j<=maxChar-i ;j++){
+            cout <<" ";
+        }
+        for(int j = maxChar-i+1; j<=maxChar ; j++){
+            cout<<--number;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
