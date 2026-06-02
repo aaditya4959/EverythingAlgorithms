71960f692443270a9d3f89c827541ce3a684a42f
Aaditya Bir Singh <aadi.bir.singh@gmail.com>
2026-06-03 00:14:27 +0530

    3 done

--- basic-recursions/recursions.cpp
+++ basic-recursions/recursions.cpp
@@ -40,9 +40,26 @@ int factorial(int N){
 }
 
 
+void reverseArray(int arr[], int start, int end) {
+    if (start >= end) {
+        return;
+    }
+
+    swap(arr[start], arr[end]);
+
+    reverseArray(arr, start + 1, end - 1);
+}
+
+
 
 
 int main(){
-    cout<<factorial(5)<<endl;
+        int arr[] = {1, 2, 3, 4, 5};
+
+    reverseArray(arr, 0, 4);
+
+    for (int x : arr) {
+        cout << x << " ";
+    }
     return 0;
 }
