1498d4e69207f7942f9bcd71b045b10a01f73084
Aaditya Bir Singh <aadi.bir.singh@gmail.com>
2026-06-03 00:03:17 +0530

    2 Done

--- basic-recursions/recursions.cpp
+++ basic-recursions/recursions.cpp
@@ -32,10 +32,17 @@ int sumOfN(int N){
     return  N + sumOfN(N-1);
 }
 
+int factorial(int N){
+    if(N == 0 || N == 1){
+        return N;
+    }
+    return N*factorial(N-1);
+}
+
 
 
 
 int main(){
-    cout<<sumOfN(5)<<endl;
+    cout<<factorial(5)<<endl;
     return 0;
 }
